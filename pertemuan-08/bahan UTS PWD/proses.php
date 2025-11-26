3bbb51c7d520f9b38f44145bdce670f86cb32068
Raka Pradipta <2511500008@mahasiswa.atmaluhur.ac.id>
2025-11-26 11:06:24 +0700

    mengedit proses php baris 10-19 menjadi bentuk array associative

--- pertemuan-08/bahan UTS PWD/proses.php
+++ pertemuan-08/bahan UTS PWD/proses.php
@@ -6,5 +6,20 @@ $sespesan = $_POST["txtPesan"];
 $_SESSION["sesnama"] = $sesnama;
 $_SESSION["sesemail"] = $sesemail;
 $_SESSION["sespesan"] = $sespesan;
+
 header("location: index.php");
+$nim        = $_SESSION["nim"]        ?? "";
+$nama       = $_SESSION["nama"]       ?? "";
+$tempat     = $_SESSION["tempat"]     ?? "";
+$tgl_lahir  = $_SESSION["tgl_lahir"]  ?? "";
+$hobi       = $_SESSION["hobi"]       ?? "";
+$pasangan   = $_SESSION["pasangan"]   ?? "";
+$pekerjaan  = $_SESSION["pekerjaan"]  ?? "";
+$ortu       = $_SESSION["ortu"]       ?? "";
+$kakak      = $_SESSION["kakak"]      ?? "";
+$adik       = $_SESSION["adik"]       ?? "";
+
+$sesnama  = $_SESSION["sesnama"]  ?? "";
+$sesemail = $_SESSION["sesemail"] ?? "";
+$sespesan = $_SESSION["sespesan"] ?? "";
 ?>
