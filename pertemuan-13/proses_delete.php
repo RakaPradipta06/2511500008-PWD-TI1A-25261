3bacad698c4adcd72805e3a6f4e688726b370c59
Raka Pradipta <2511500008@mahasiswa.atmaluhur.ac.id>
2025-12-24 11:59:39 +0700

    menambahkan parameter cid untuk link delete di read.php

--- pertemuan-13/proses_delete.php
+++ pertemuan-13/proses_delete.php
@@ -3,11 +3,7 @@
   require __DIR__ . './koneksi.php';
   require_once __DIR__ . '/fungsi.php';
 
-  #cek method form, hanya izinkan POST
-  if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    $_SESSION['flash_error'] = 'Akses tidak valid.';
-    redirect_ke('read.php');
-  }
+
 
   #validasi cid wajib angka dan > 0
 //   $cid = filter_input(INPUT_POST, 'cid', FILTER_VALIDATE_INT, [
@@ -24,12 +20,13 @@
     menyiapkan query UPDATE dengan prepared statement 
     (WAJIB WHERE cid = ?)
   */
-  $stmt = mysqli_prepare($conn, "DELETE FROM tbl_tamu");                           
+  $stmt = mysqli_prepare($conn, "DELETE FROM tbl_tamu"); 
                                 #WHERE cid = ?");
   if (!$stmt) {
     #jika gagal prepare, kirim pesan error (tanpa detail sensitif)
     $_SESSION['flash_error'] = 'Terjadi kesalahan sistem (prepare gagal).';
-    redirect_ke('read.php');  }
+    redirect_ke('read.php');
+  }
 
   #bind parameter dan eksekusi (s = string, i = integer)
   #mysqli_stmt_bind_param($stmt, "sssi", $nama, $email, $pesan, $cid);
@@ -39,9 +36,9 @@
     /*
       Redirect balik ke read.php dan tampilkan info sukses.
     */
-    $_SESSION['flash_sukses'] = 'Terima kasih, data Anda sudah dibuang.';
+    $_SESSION['flash_sukses'] = 'Terima kasih, data Anda sudah dihapus.';
   } else { #jika gagal, simpan kembali old value dan tampilkan error umum
-    $_SESSION['flash_error'] = 'Data gagal diperbaharui. Silakan coba lagi.';
+    $_SESSION['flash_error'] = 'Data gagal dhapus. Silakan coba lagi.';
   }
   #tutup statement
   mysqli_stmt_close($stmt);
